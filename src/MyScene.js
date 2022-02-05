8d6f283bccf4a4c7914ff11cd2b9d818c3df7c1c
salaadas <carbonfibersweeper@gmail.com>
2022-02-05 14:59:04 +0700

    default scene class

---
+++ src/MyScene.js
@@ -0,0 +1,56 @@
+import * as THREE from 'three';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
+import Stats from 'three/examples/jsm/libs/stats.module.js';
+
+export default class MyScene {
+  constructor(fov = 36, camera, scene, stats, controls, renderer) {
+    this.fov = fov;
+    this.camera = camera;
+    this.stats = stats;
+    this.scene = scene;
+    this.controls = controls;
+    this.renderer = renderer;
+  }
+
+  init() {
+    this.camera = new THREE.PerspectiveCamera(
+      this.fov,
+      window.innerWidth / window.innerHeight,
+      1,
+      1000
+    );
+    this.camera.position.z = 128;
+
+    this.scene = new THREE.Scene();
+
+    this.renderer = new THREE.WebGLRenderer({
+      canvas: document.querySelector('canvas.webgl'),
+      antialias: true,
+    });
+    this.renderer.setSize(window.innerWidth, window.innerHeight);
+    document.body.appendChild(this.renderer.domElement);
+
+    this.controls = new OrbitControls(this.camera, this.renderer.domElement);
+
+    this.stats = Stats();
+    document.body.appendChild(this.stats.dom);
+
+    window.addEventListener('resize', this.handleResize, false);
+  }
+
+  animate() {
+    window.requestAnimationFrame(this.animate.bind(this));
+    this.render();
+    this.stats.update();
+  }
+
+  render() {
+    this.renderer.render(this.scene, this.camera);
+  }
+
+  handleResize() {
+    this.camera.aspect = window.innerWidth / window.innerHeight;
+    this.camera.updateProjectionMatrix();
+    this.renderer.setSize(window.innerWidth, window.innerHeight);
+  }
+}
